a5cd742beba4a181dbfdab5a7ef0f0442f420300
Jheonbeo <bee9105@gmail.com>
2019-07-11 10:29:07 +0900

    2019.07.11 controller

--- src/main/java/org/zerock/controller/HomeController.java
+++ src/main/java/org/zerock/controller/HomeController.java
@@ -1,39 +1,46 @@
 package org.zerock.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.zerock.mapper.TimeMapper;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-	
+	@Setter(onMethod_= {@Autowired})
+	private TimeMapper timeMapper;
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
+	//home을 리턴했으니 경로는 '/WEB-INF/views/home.jsp'가 된다. '/WEB-INF/views/*.jsp'라 Servlet에 지정했으니
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
+//		Date date = new Date();
+//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+//		
+//		String formattedDate = dateFormat.format(date);
+//		
+//		model.addAttribute("serverTime", formattedDate );
+		model.addAttribute("serverTime", timeMapper.getTime2() );
 		
 		return "home";
 	}
-	
 }
---
+++ src/main/java/org/zerock/domain/SampleDTO.java
@@ -0,0 +1,12 @@
+package org.zerock.domain;
+
+import java.util.ArrayList;
+
+import lombok.Data;
+
+@Data
+public class SampleDTO {
+	int Age;
+	String Name;
+	ArrayList<String> ids;
+}
