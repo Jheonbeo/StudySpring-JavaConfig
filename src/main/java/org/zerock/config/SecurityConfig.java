c8a84fc41d6fe0496e8dae8dba8057feb24c2d15
takata <takata@jheonbeo-PC>
2021-09-13 13:37:34 +0900

    2021.09.13 Fixed error

--- src/main/java/org/zerock/config/SecurityConfig.java
+++ src/main/java/org/zerock/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
